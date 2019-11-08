5a3dc656221e0b23ad008694ceaf3b6d1e8dae90
librallaw <liballaw@yahoo.com>
2019-11-08 09:07:12 +0100

    Added create Personnel and view Personnel

--- app/Http/Controllers/Call/CallListController.php
+++ app/Http/Controllers/Call/CallListController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-
+use Validator;
 class CallListController extends Controller
 {
     //
